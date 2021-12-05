9fa88f8c036d716b38e30614a42cda87144cd952
Abhishek Srivastava <abhi.srivastava7631@gmail.com>
2021-12-04 22:34:32 -0800

    ok

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -17,7 +17,7 @@ export class EmployeesComponent implements OnInit {
       { id:1,name:'iamneo1',city:'mumbai',exp:10,joined:new Date(2014,7,16)},
       { id:2,name:'iamneo2',city:'chennai',exp:2,joined:new Date(2014,9,16)},
       { id:3,name:'iamneo3',city:'pune',exp:20,joined:new Date(2014,11,16)},
-      { id:4,name:'iamneo4',city:'Bengalaru',exp:11,joined:new Date(2014,12,16)}
+      { id:4,name:'iamneo4',city:'Bengaru',exp:11,joined:new Date(2014,12,16)}
     ]
   }
 
